3e5a1a18c986641d38fd4b9053f84923ae66ec65
Roko Sango <sangor96@univie.ac.at>
2023-05-12 20:03:45 +0200

    first commit

---
+++ scripts/02_QC_and_expl_analysis.R
@@ -0,0 +1,30 @@
+
+#Part 1: cellranger pipeline was done in UNIX. now comes analysis in R.
+
+#Part 2: Quality control and exploratory analysis 
+#We will now check the quality of the data further:
+
+# 1. Mapping quality
+# 2. Cell counts
+# 3. Distribution of keys quality metrics
+
+#We will then:
+
+# 1. Filter genes with very low expression
+# 2. Identify low-quality cells
+# 3. Filter and/or mark low quality cells
+
+library(DropletUtils)
+library(scater)
+library(ensembldb)
+library(AnnotationHub)
+library(BiocParallel)
+library(tidyverse)
+library(patchwork)
+library(ggvenn)
+
+path = '/scratch/cube/sango/sarcoidosis_project'
+setwd(path)
+
+
+#loading a single sample
